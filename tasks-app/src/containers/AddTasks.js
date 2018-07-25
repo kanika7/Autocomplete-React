b9ec6a3b034d4aa245b634b42a1301fb8537657a
kanika.07 <kanika.bhargava89@gmail.com>
2018-07-25 17:12:10 +0530

    tasks-app

---
+++ tasks-app/src/containers/AddTasks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddTasks = () => {
+    return(
+        <div class="container"> 
+            <form>
+                <textarea></textarea>
+                <button type="submit">Add Task</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTasks
